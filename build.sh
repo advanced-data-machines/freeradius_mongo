783ccfcefccacd5036a8cf6a1cbbeaaff38fdb79
sjbeskur <sam.beskur@gmail.com>
2020-11-04 17:05:24 -0700

    add example / doco

--- build.sh
+++ build.sh
@@ -2,15 +2,17 @@
 
 docker build -t mongo_rlm_build .
 
+mkdir target/
+
 CID=$(docker create mongo_rlm_build)
-docker cp ${CID}:/usr/lib/rlm_sql_mongo.a lib/
-docker cp ${CID}:/usr/lib/rlm_sql_mongo.la lib/
-docker cp ${CID}:/usr/lib/rlm_sql_mongo.so lib/
+docker cp ${CID}:/usr/lib/rlm_sql_mongo.a target/
+docker cp ${CID}:/usr/lib/rlm_sql_mongo.la target/
+docker cp ${CID}:/usr/lib/rlm_sql_mongo.so target/
 
 docker rmi ${CID}
 echo 
 echo "Copy these files to /usr/lib/freeradius directory "
-ls -l lib/
+ls -l target/
 
 echo 
 echo "COPY lib/rlm_sql_mongo* /usr/lib/freeradius/"
